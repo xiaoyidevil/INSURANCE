05e51a7fa57d4d62aca1978d38b3a130d13e5752
xiaoyidevil <ltt.007@163.com>
2021-04-12 21:16:26 +0800

    add boat owner table

--- DATA/INSURANCE/INSURANCE/DDL_tbl_InsurancePolicy.sql
+++ DATA/INSURANCE/INSURANCE/DDL_tbl_InsurancePolicy.sql
@@ -1,6 +1,6 @@
-IF (EXISTS(SELECT * FROM SYSOBJECTS WHERE NAME = 'tbl_InsurancePolicy' AND XTYPE = 'U')) 
+IF (EXISTS(SELECT * FROM sys.sysobjects WHERE NAME = 'tbl_InsurancePolicy' AND XTYPE = 'U')) 
 BEGIN
-	DROP TABLE tbl_InsurancePolicy
+	DROP TABLE dbo.tbl_InsurancePolicy
 END
 GO
 
@@ -11,43 +11,88 @@ SET QUOTED_IDENTIFIER ON
 GO
 
 CREATE TABLE [dbo].[tbl_InsurancePolicy](
-	[ID]					[int],
-	[CompanyID]				[int],
-	[CompanyName]			[nvarchar](50),
-	[EffectiveDateStart]	[datetime],
-	[EffectiveDateEnd]		[datetime],
-	[ShipOwnerID]			[int],
-	[ShipOwnerName]			[nvarchar](50),
-	[ShipID]				[int],
-	[ShipNo]				[nvarchar](50),
-	[ShipName]				[nvarchar](50),
-	[DateCreated]			[datetime] NULL,
-	[CreatedBy]				[nvarchar](20) NULL,
-	[DateLastUpdated]		[datetime] NULL,
-	[LastUpdatedBy]			[nvarchar](50) NULL,
+	[ID]					INT IDENTITY(1,1) NOT NULL,
+	[CompanyName]			NVARCHAR(50) NOT NULL,
+	[BoatNum]				INT NULL,
+	[BoatOwnerName]			NVARCHAR(50) NULL,
+	[EffectiveDateStart]	DATETIME NULL,
+	[EffectiveDateEnd]		DATETIME NULL,
+	[PersonInsuranced]		NVARCHAR(10) NOT NULL,
+	[Identity]				NVARCHAR(18) NOT NULL,
+	[Industry]				NVARCHAR(10) NOT NULL,
+	[DateCreated]			DATETIME NULL,
+	[CreatedBy]				NVARCHAR(20) NULL,
+	[DateLastUpdated]		DATETIME NULL,
+	[LastUpdatedBy]			NVARCHAR(50) NULL,
 CONSTRAINT [PK_tbl_InsuramcePolicy] PRIMARY KEY CLUSTERED
 (
 	ID ASC,
-	CompanyID ASC
+	[CompanyName] ASC
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
 ) ON [PRIMARY]
 GO
 
-EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'投保单位id' , @level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'TABLE',@level1name=N'tbl_InsurancePolicy', @level2type=N'COLUMN',@level2name=N'CompanyID'
+EXEC sys.sp_addextendedproperty @name = N'MS_Description',
+                                @value = N'投保单位名称',
+                                @level0type = N'SCHEMA',
+                                @level0name = N'dbo',
+                                @level1type = N'TABLE',
+                                @level1name = N'tbl_InsurancePolicy',
+                                @level2type = N'COLUMN',
+                                @level2name = N'CompanyName';
 GO
-EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'投保单位名称' , @level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'TABLE',@level1name=N'tbl_InsurancePolicy', @level2type=N'COLUMN',@level2name=N'CompanyName'
+EXEC sys.sp_addextendedproperty @name = N'MS_Description',
+                                @value = N'船号',
+                                @level0type = N'SCHEMA',
+                                @level0name = N'dbo',
+                                @level1type = N'TABLE',
+                                @level1name = N'tbl_InsurancePolicy',
+                                @level2type = N'COLUMN',
+                                @level2name = N'BoatNum';
 GO
-EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'保单有效期开始日' , @level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'TABLE',@level1name=N'tbl_InsurancePolicy', @level2type=N'COLUMN',@level2name=N'EffectiveDateStart'
+EXEC sys.sp_addextendedproperty @name = N'MS_Description',
+                                @value = N'船主姓名',
+                                @level0type = N'SCHEMA',
+                                @level0name = N'dbo',
+                                @level1type = N'TABLE',
+                                @level1name = N'tbl_InsurancePolicy',
+                                @level2type = N'COLUMN',
+                                @level2name = N'BoatOwnerName';
 GO
-EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'保单有效期截止日' , @level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'TABLE',@level1name=N'tbl_InsurancePolicy', @level2type=N'COLUMN',@level2name=N'EffectiveDateEnd'
+EXEC sys.sp_addextendedproperty @name = N'MS_Description',
+                                @value = N'保单有效期开始日',
+                                @level0type = N'SCHEMA',
+                                @level0name = N'dbo',
+                                @level1type = N'TABLE',
+                                @level1name = N'tbl_InsurancePolicy',
+                                @level2type = N'COLUMN',
+                                @level2name = N'EffectiveDateStart';
 GO
-EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'船主ID' , @level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'TABLE',@level1name=N'tbl_InsurancePolicy', @level2type=N'COLUMN',@level2name=N'ShipOwnerID'
+EXEC sys.sp_addextendedproperty @name = N'MS_Description',
+                                @value = N'保单有效期截止日',
+                                @level0type = N'SCHEMA',
+                                @level0name = N'dbo',
+                                @level1type = N'TABLE',
+                                @level1name = N'tbl_InsurancePolicy',
+                                @level2type = N'COLUMN',
+                                @level2name = N'EffectiveDateEnd';
 GO
-EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'船主名称' , @level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'TABLE',@level1name=N'tbl_InsurancePolicy', @level2type=N'COLUMN',@level2name=N'ShipOwnerName'
+EXEC sys.sp_addextendedproperty @name = N'MS_Description',
+                                @value = N'被保险人姓名',
+                                @level0type = N'SCHEMA',
+                                @level0name = N'dbo',
+                                @level1type = N'TABLE',
+                                @level1name = N'tbl_InsurancePolicy',
+                                @level2type = N'COLUMN',
+                                @level2name = N'PersonInsuranced';
 GO
-EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'船只ID' , @level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'TABLE',@level1name=N'tbl_InsurancePolicy', @level2type=N'COLUMN',@level2name=N'ShipID'
+EXEC sys.sp_addextendedproperty @name = N'MS_Description',
+                                @value = N'身份证号',
+                                @level0type = N'SCHEMA',
+                                @level0name = N'dbo',
+                                @level1type = N'TABLE',
+                                @level1name = N'tbl_InsurancePolicy',
+                                @level2type = N'COLUMN',
+                                @level2name = N'Identity';
 GO
-EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'船只编号' , @level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'TABLE',@level1name=N'tbl_InsurancePolicy', @level2type=N'COLUMN',@level2name=N'ShipNo'
-GO
-EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'船只名称' , @level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'TABLE',@level1name=N'tbl_InsurancePolicy', @level2type=N'COLUMN',@level2name=N'ShipName'
-GO
+
