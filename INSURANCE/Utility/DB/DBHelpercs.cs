707d3ef21e267c1090fb90dac310b657761504a7
xiaoyidevil <ltt.007@163.com>
2021-04-16 17:39:50 +0800

    Add files via upload

---
+++ INSURANCE/Utility/DB/DBHelpercs.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XYYANG.Web.Utility.DB
+{
+    public class DBHelpercs:IDisposable
+    {
+        private bool IsDisposed = false;
+
+        private SqlConnection _sqlConnection = null;
+        public SqlConnection sqlConnection
+        {
+            get { return _sqlConnection; }
+            set { _sqlConnection = value; }
+        }
+ 
+
+        private SqlCommand _sqlCommand = null;
+        public SqlCommand sqlCommand
+        {
+            get { return _sqlCommand; }
+            set { _sqlCommand = value; }
+        }
+        
+
+        private SqlTransaction _sqlTransaction = null;
+        public SqlTransaction sqlTransaction
+        {
+            get { return _sqlTransaction; }
+            set { _sqlTransaction = value; }
+        }
+        
+        public string ConnectionString {
+            get {
+                if (_sqlConnection != null)
+                {
+                    return _sqlConnection.ConnectionString;
+                }
+                else
+                {
+                    return "";
+                }
+            }
+
+            set {
+                if (_sqlConnection != null)
+                {
+                    if (_sqlConnection.State == System.Data.ConnectionState.Open)
+                    {
+                        _sqlConnection.Close();
+                    }
+                    _sqlConnection.Dispose();
+                }
+                _sqlConnection = new SqlConnection(value);
+
+            }
+        }
+
+        public DBHelpercs()
+        { 
+        
+        }
+
+        public DBHelpercs(string sqlConnection)
+        {
+            this.ConnectionString = sqlConnection;
+        }
+
+        ~DBHelpercs()
+        {
+            Dispose(false);
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected void Dispose(bool Disposing)
+        {
+            if (!IsDisposed)
+            {
+                if (Disposing)
+                {
+                    //clean up managed resources
+                }
+                // clean up unmanaged resources
+            }
+            IsDisposed = true;
+        }
+
+        public bool ExecuteNonQuery(string sql, SqlParameter[] sqlPara)
+        {
+            bool blnResult = false;
+
+            if(_sqlConnection
+
+            return blnResult;
+        }
+    
+    }
+}
---
+++ INSURANCE/Utility/Excel/ExcelHelper.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.OleDb;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XYYANG.Web.Utility.Excel
+{
+    public class ExcelHelper
+    {
+        public enum ExcelColumnType
+        {
+            //ExcelString = "System.String",
+            //ExcelInt = "System.Int16",
+            //ExcelDate = "System.DateTime"
+        }
+
+        private string _ConnectionString = "";
+
+        private string _FilePath= "";
+        public string FilePath { 
+            get{
+                return _FilePath;
+            }
+            set{
+                _FilePath = value;
+                _ConnectionString = @"Provider=Microsoft.ACE.OLEDB.12.0; Data Source=""" + _FilePath + @"""; Extended Properties=Excel 8.0;";
+            }
+        }
+
+
+        Dictionary<string, string> _dicColumns = new Dictionary<string,string>();
+        public Dictionary<string,string> dicColumns{
+            get {
+                return _dicColumns;
+            }
+            set {
+                if (value != null)
+                {
+                    foreach (var item in value)
+                    {
+                        _dicColumns.Add(item.Key.ToString(), item.Value.ToString());
+                    }
+                }
+            }
+        }
+
+
+        public ExcelHelper()
+        { 
+            
+        }
+
+        public ExcelHelper(string strFilePath)
+        {
+            this.FilePath = strFilePath;
+        }
+
+        public DataTable readWholeExcelToDataTable()
+        {
+            DataTable dtResult = new DataTable();
+
+            OleDbConnection excelConnection = new OleDbConnection(_ConnectionString);
+            OleDbCommand excelCommand = new OleDbCommand("SELECT * FROM [Sheet1$]", excelConnection);
+            OleDbDataAdapter excelAdapter = new OleDbDataAdapter(excelCommand);
+            try
+            {
+                excelConnection.Open();
+                excelAdapter.Fill(dtResult);
+
+            }
+            catch (Exception e)
+            {
+
+                //throw;
+            }
+            finally {
+                if (excelCommand != null)
+                {
+                    excelCommand.Dispose();
+                }
+
+                if (excelConnection != null) {
+                    if (excelConnection.State != ConnectionState.Closed)
+                    {
+                        excelConnection.Close();
+                    }
+                    excelConnection.Dispose();
+                }
+            }
+
+            return dtResult;
+        }
+
+                public DataTable readExcelToDataTable()
+        {
+            DataTable dtResult = new DataTable();
+            try
+            {
+                if (_dicColumns.Count > 0)
+                {
+                    foreach (var item in _dicColumns)
+                    {
+                        dtResult.Columns.Add(item.Key, Type.GetType(item.Value.ToString()));
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+
+            }
+
+            OleDbConnection excelConnection = new OleDbConnection(_ConnectionString);
+            OleDbCommand excelCommand = new OleDbCommand("SELECT * FROM [Sheet1$]", excelConnection);
+            OleDbDataReader excelReader =null ;
+            
+            try
+            {
+                excelConnection.Open();
+                excelReader = excelCommand.ExecuteReader();
+
+                while (excelReader.Read())
+                {
+                    DataRow excelDataRow = dtResult.NewRow();
+
+                    foreach (DataColumn item in dtResult.Columns)
+                    {
+                        excelDataRow[item.ColumnName] = excelReader[item.ColumnName];
+                    }
+
+                    dtResult.Rows.Add(excelDataRow);
+                }
+
+                if (excelReader != null)
+                {
+                    excelReader.Close();
+                    excelReader.Dispose();
+                }
+            }
+            catch (Exception e)
+            {
+
+                //throw;
+            }
+            finally {
+                if (excelReader != null)
+                {
+                    excelReader.Close();
+                    excelReader.Dispose();
+                }
+
+                if (excelCommand != null)
+                {
+                    excelCommand.Dispose();
+                }
+
+                if (excelConnection != null) {
+                    if (excelConnection.State != ConnectionState.Closed)
+                    {
+                        excelConnection.Close();
+                    }
+                    excelConnection.Dispose();
+                }
+            }
+
+            return dtResult;
+        }
+
+    }
+}
