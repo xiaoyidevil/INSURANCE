a7c3e9759ff04e96cfefa73f740939615e6f4621
xiaoyidevil <ltt.007@163.com>
2021-04-25 17:04:33 +0800

    Add files via upload

---
+++ INSURANCE/INSURANCE/Scripts/common.js
@@ -0,0 +1,10 @@
+﻿function showStartCalender()
+{
+    var calStartDate = document.getElementById("FeaturedContent_calStartDate");
+    calStartDate.style.visibility = "visible";
+}
+
+function showEndCalender() {
+    var calEndDate = document.getElementById("FeaturedContent_calEndDate");
+    calEndDate.style.visibility = "visible";
+}
